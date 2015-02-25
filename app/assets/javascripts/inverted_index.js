5a4374a80ca0a8ebbe5cd9c365cb5834a28cb99f
Clayton Albachten <clay.reed.a@gmail.com>
2015-02-25 15:57:01 -0500

    Test $http w/ $scope.consoleLogListings

--- app/assets/javascripts/inverted_index.js
+++ app/assets/javascripts/inverted_index.js
@@ -1,4 +1,73 @@
-console.log('I load!');
+/* InvertedIndex APP */
 
-var invertedIndex = angular.module('invertedIndex', 
-  []);
+var invertedIndex = angular.module('invertedIndex', [
+  'controllers',
+  'services']
+);
+
+/* SERVICES */
+
+var services = angular.module('services', []);
+
+services.service('Listings', 
+  function($http){
+    this.searchListings = function(queryString){
+      return $http(
+        {
+          method: 'GET', 
+          url: '/api/listings/s.json',
+          params: {
+            q: queryString
+          }
+        }
+      );
+
+    }
+
+    this.indexListings = function(){
+      return $http(
+        {
+          method: 'GET', 
+          url: '/api/listings.json'
+        }
+      );
+    }
+
+    this.showListing = function(listingId){
+      return $http(
+        {
+          method: 'GET', 
+          url: '/api/listings/'+listingId+'.json'
+        }
+      );
+    }
+
+    return this; 
+  });
+
+/* CONTROLLERS */
+
+var controllers = angular.module('controllers', []);
+
+controllers.controller('MainController', 
+  function($scope, Listings){
+    $scope.queryString = '';
+    $scope.searchResults = null; 
+
+    $scope.consoleLogListings = function(){
+      Listings.indexListings()
+        .then(
+          function(data, status, config, headers)
+          {
+            for (var i = 0; i < data.data.length; i++) {
+              console.log(data.data[i].title); 
+            }
+          },
+          function(data, status, config, headers)
+          {
+            console.log('Problems!');
+          }
+        )
+    };
+  }
+);
