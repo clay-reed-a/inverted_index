00d99cd77d07d1d3da64bd8ca0f300d0285c1d26
Clayton Albachten <clay.reed.a@gmail.com>
2015-02-27 17:57:20 -0500

    Rank results by word frequency

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,9 +7,14 @@ class ListingsController < ApplicationController
     words = query_string.downcase.split ' '
 
     word_models = words.map { |word| Word.find_by(content: word) }
-    listings_for_words = word_models.map &:uniq_listings
+    entries_for_words = word_models.map &:entries
+    listings_for_words = entries_for_words.map { |entries|
+      rank_results_by_frequency(entries)  
+    }
+
     listings_with_all_words = listings_for_words.reduce &:&
-    @listings = listings_with_all_words 
+    
+    @listings = listings_with_all_words
     
     render :index 
   end
@@ -21,4 +26,24 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+    private 
+
+    def rank_results_by_frequency entries
+      # takes an array of Entries & 
+      # returns an array of unique Listings, 
+      # ordered by the number of 
+      # Entries in the original array 
+      # belonging to each Listing
+    
+      histogram = 
+        entries.reduce(Hash.new(0)) { |hash, entry| 
+          hash[entry.listing_id] += 1; hash
+      }
+
+      entries.sort_by { |entry|
+        [histogram[entry.listing_id] * -1, entry]  
+      }.map(&:listing).uniq 
+
+    end 
 end
